c6edbcccab14b0c2dade736f22074dc7d6ded456
Sergey <61414420+SerchsOff@users.noreply.github.com>
2020-03-22 13:54:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 var head = document.querySelector("header");
 let name1 = document.querySelector(".name1-active");
-let name2 = document.querySelector(".name2-active")
+let name2 = document.querySelector(".name2-active");
+let placehold = document.querySelector(".placehold")
 
 name1.classList.remove("name1-active")
 name2.classList.remove("name2-active")
+
+function placeh() {
+	setTimeout(() => {
+		placehold.style.opacity = "0"
+		setTimeout(() => {
+			placehold.style.display = "none"
+		},1000)
+	},1000)
+}
+
+window.onload = function() {
+	placeh();
+	scroll()
+}
 
 window.onscroll = function() {
 	scroll()
