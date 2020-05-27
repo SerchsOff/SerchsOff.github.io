2b781119195410b1252a2aa6b77b42c60971ff01
Sergey <61414420+SerchsOff@users.noreply.github.com>
2020-05-27 14:09:32 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,8 +1,12 @@
 <?php 
 	$name = $_POST["name"];
 	$email = $_POST["email"];
-	$message = $_POST["message"];
-	if (mail("rogovsergey2005@gmail.com", "Заявка на работу", "имя:" . $name . ", " . $message,"From:". $email . "\r\n")) {
+	$message = $_POST["message
+	$headers  = "From: " . strip_tags($email) . "\r\n";
+	$headers .= "Reply-To: ". strip_tags($email) . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/plain;charset=utf-8 \r\n";
+	if (mail("rogovsergey2005@gmail.com", "Заявка на работу", "имя:" \n . $name . ", " . $message, $headers)) {
 		echo "Сообщение отправлено";
 	};
 ?>
