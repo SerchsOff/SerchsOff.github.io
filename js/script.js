0e51ecf9483f891a67f640070ada112e244f6f03
Sergey <61414420+SerchsOff@users.noreply.github.com>
2020-08-19 23:13:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,14 @@
 var head = document.querySelector("header");
-let name1 = document.querySelector(".name1-active");
-let name2 = document.querySelector(".name2-active");
-let placehold = document.querySelector(".placehold")
-
-name1.classList.remove("name1-active")
-name2.classList.remove("name2-active")
-
-function placeh() {
-	setTimeout(() => {
-		placehold.style.opacity = "0"
-		setTimeout(() => {
-			placehold.style.display = "none"
-		},1000)
-	},1000)
-}
 
 window.onload = function() {
-	placeh();
-	scroll()
+	scrollGet()
 }
 
 window.onscroll = function() {
-	scroll()
+	scrollGet()
 }
 
-function scroll() {
+function scrollGet() {
 	if (window.pageYOffset >= 300) {
 		head.style.position = "fixed"
 		head.style.background = "#000"
